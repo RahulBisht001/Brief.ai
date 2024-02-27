fbc0be1c809f7ad87b1c2de95fb4b6d2734e3db6
RahulB <rahulbisht1012@gmail.com>
2024-02-27 15:36:37 +0530

    Vercel Analytics Added
    learnt about vercel analytics and added the vercel
    analytics in the app to track the user activities.

--- app/page.js
+++ app/page.js
@@ -6,6 +6,14 @@ export default function Home() {
             <div>
                 <h1 className="text-3xl sm:text-4xl font-Poppins font-semibold p-4 tracking-wider">Brief.ai</h1>
             </div>
+            <div className="mx-auto max-w-5xl text-center mt-10">
+                {/* <h1 className="font-Poppins bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-8xl">
+                    Summarize Text Seamlessly
+                </h1> */}
+                {/* <h1 className="font-Poppins bg-gradient-to-r from-purple-700 via-blue-500 to-green-500  bg-clip-text text-4xl font-extrabold text-transparent sm:text-8xl">
+                    Summarize Text Seamlessly
+                </h1> */}
+            </div>
         </>
     );
 }
